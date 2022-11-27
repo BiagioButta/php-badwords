7d3e066ea45015c21fff1717ef09d6cfc6b36620
BiagioButta <biagiob940@gmail.com>
2022-11-27 18:08:06 +0100

    completo senza css

--- index.php
+++ index.php
@@ -6,13 +6,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <form action="secondo.php" method="GET">
-        <input type="text" name="nome" placeolder="Inserisci nome">
-        <button type="submit">Invia</button>
+
+<div class="container">
+
+    <form action="./secondo.php" method="POST">
+
+        <div class="row row-cols-1 justify-content-center align-items-center">
+            <div class="col d-flex justify-content-center"><input type="text" name="bannedWord"></div>
+            <div class="col d-flex justify-content-center"><textarea name="txt" id="" cols="30" rows="10"></textarea></div>
+            <div class="col d-flex justify-content-center"><button type="submit">Vai</button></div>
+        </div>
 
     </form>
+
+</div>
 </body>
 </html>
--- secondo.php
+++ secondo.php
@@ -1,5 +1,12 @@
 <?php
-    $nome = $_GET['nome'];
+
+    $bannedWord = $_POST['bannedWord'];
+    $txt = $_POST['txt']; 
+
+    if(isset($_POST['bannedWord'])){
+        $censoredTxt = str_ireplace($bannedWord, '***', $txt);
+    };
+
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +15,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <h1><?php echo "Ciao $nome" ?></h1>
+    <div>Lunghezza txt <?php echo strlen($txt); ?></div>
+    <div>Frase censurata <?php echo $censoredTxt; ?></div>
 </body>
 </html>
